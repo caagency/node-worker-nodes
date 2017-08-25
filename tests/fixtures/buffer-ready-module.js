4a72d26a441a86c94a672c80bf59541f7a8ea30a
Sanjay Pandit <spandit@caa.com>
2017-08-25 10:30:29 -0700

    remove support for buffers

--- tests/fixtures/buffer-ready-module.js
+++
@@ -1,8 +0,0 @@
-const zlib = require('zlib');
-
-module.exports = {
-    getDeflated: (msg = 'Hello!') => zlib.deflateSync(msg),
-    getDeflatedInProperty: (msg = 'Hello!') => ({ info: 'deflate', data: zlib.deflateSync(msg) }),
-    isBuffer: arg => Buffer.isBuffer(arg),
-    sliceInHalf: buffer => buffer.slice(0, Math.floor(buffer.length / 2))
-};
